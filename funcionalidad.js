e0befb08885811b2cd85b315d17e9d72357011ca
mct1o <120459625+mct1o@users.noreply.github.com>
2023-03-07 12:41:08 -0300

    initial commit

---
+++ funcionalidad.js
@@ -0,0 +1,29 @@
+'use strict';
+let mensaje
+
+
+
+
+       window.addEventListener("load",function(){
+        document.getElementById("linkedin").addEventListener("click",function(){
+            mensaje="www.linkedin.com"
+            alert(mensaje)
+        })
+    })
+
+        window.addEventListener("load",function(){
+            document.getElementById("telefono").addEventListener("click",function(){
+                mensaje="+54123456789"
+                alert(mensaje)
+            })
+
+        })
+
+        window.addEventListener("load",function(){
+            document.getElementById("gmail").addEventListener("click",function(){
+                mensaje="Usuario@ejemplo.com"
+                alert(mensaje)
+            })
+            })
+
+       
